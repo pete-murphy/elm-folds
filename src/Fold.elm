2893e13ddbc1c6427280186061720efc5fc6492c
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2026-06-06 12:20:31 -0400

    feat: more basic folds

--- src/Fold.elm
+++ src/Fold.elm
@@ -65,6 +65,130 @@ scanList xs fold =
     List.map extract (List.Extra.scanl (\a (Fold o) -> o.step a) fold xs)
 
 
+{-| A `Fold` which remembers the first input.
+-}
+head : Fold a (Maybe a)
+head =
+    unfoldFold_ Nothing
+        (\a m ->
+            case m of
+                Just _ ->
+                    m
+
+                Nothing ->
+                    Just a
+        )
+
+
+{-| A `Fold` which keeps the last input.
+-}
+last : Fold a (Maybe a)
+last =
+    unfoldFold_ Nothing (\a _ -> Just a)
+
+
+{-| A `Fold` which tests whether any inputs were seen.
+-}
+null : Fold a Bool
+null =
+    unfoldFold_ True (\_ _ -> False)
+
+
+{-| A `Fold` which counts its inputs.
+-}
+length : Fold a number
+length =
+    unfoldFold_ 0 (\_ n -> n + 1)
+
+
+{-| A `Fold` which tests if _all_ of its inputs were true.
+-}
+and : Fold Bool Bool
+and =
+    unfoldFold_ True (&&)
+
+
+{-| A `Fold` which tests if _any_ of its inputs were true.
+-}
+or : Fold Bool Bool
+or =
+    unfoldFold_ False (||)
+
+
+{-| A `Fold` which tests if _all_ of its inputs satisfy some predicate.
+-}
+all : (a -> Bool) -> Fold a Bool
+all pred =
+    unfoldFold_ True (\a b -> b && pred a)
+
+
+{-| A `Fold` which tests if _any_ of its inputs satisfy some predicate.
+-}
+any : (a -> Bool) -> Fold a Bool
+any pred =
+    unfoldFold_ False (\a b -> b || pred a)
+
+
+{-| A `Fold` which computes the sum of its inputs.
+-}
+sum : Fold number number
+sum =
+    unfoldFold_ 0 (+)
+
+
+{-| A `Fold` which computes the product of its inputs.
+-}
+product : Fold number number
+product =
+    unfoldFold_ 1 (*)
+
+
+{-| A `Fold` which computes the maximum of its inputs, or `Nothing` if there
+were no inputs.
+-}
+maximum : Fold comparable (Maybe comparable)
+maximum =
+    unfoldFold_ Nothing
+        (\a m ->
+            case m of
+                Nothing ->
+                    Just a
+
+                Just b ->
+                    Just (max a b)
+        )
+
+
+{-| A `Fold` which computes the minimum of its inputs, or `Nothing` if there
+were no inputs.
+-}
+minimum : Fold comparable (Maybe comparable)
+minimum =
+    unfoldFold_ Nothing
+        (\a m ->
+            case m of
+                Nothing ->
+                    Just a
+
+                Just b ->
+                    Just (min a b)
+        )
+
+
+{-| A `Fold` which tests if a specific value appeared as an input.
+-}
+elem : a -> Fold a Bool
+elem a =
+    any ((==) a)
+
+
+{-| A `Fold` which tests if a specific value did not appear as an input.
+-}
+notElem : a -> Fold a Bool
+notElem a =
+    all ((/=) a)
+
+
 extract : Fold a b -> b
 extract (Fold o) =
     o.finish ()
